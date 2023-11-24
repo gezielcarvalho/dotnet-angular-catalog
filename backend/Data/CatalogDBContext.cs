5031411cd3f0c7fd1f02c01533b4dde387521637
Geziel Carvalho <geziel.natal@gmail.com>
2023-11-24 00:00:19 -0500

    add model and controler Category

---
+++ backend/Data/CatalogDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data
+{
+    public class CatalogDBContext : DbContext
+    {
+        public CatalogDBContext(DbContextOptions<CatalogDBContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; } = null!;
+    }
+}
---
+++ backend/Models/Category.cs
@@ -0,0 +1,68 @@
+﻿using Backend.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace Backend.Models
+{
+    [Table("Category")]
+    public class Category : ISerializable, IComparable<Category>
+    {
+
+        public Category() { }
+        public Category(string name)
+        {
+            Name = name;
+        }
+        public Category(int id, string name, DateTime createdAt, DateTime updatedAt)
+        {
+            Id = id;
+            Name = name;
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+        }
+
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public int CompareTo(Category? other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+            return Id.CompareTo(other.Id);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj switch
+            {
+                Category => ((Category)obj).Id == Id,
+                _ => false,
+            };
+        }
+
+        public override int GetHashCode()
+        {
+            int prime = 31;
+            int result = 1;
+            result = prime * result + Id;
+            return result;
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+
+            info.AddValue("id", Id);
+            info.AddValue("name", Name);
+            info.AddValue("createdAt", CreatedAt);
+            info.AddValue("updatedAt", UpdatedAt);
+        }
+    }
+
+
+}
