d89d3f5de105662ea7c63cfb950d99d1e8b15163
Geziel Carvalho <geziel.natal@gmail.com>
2023-12-09 19:02:19 -0500

    add header

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -10,8 +10,7 @@ interface WeatherForecast {
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
   public forecasts: WeatherForecast[] = [];
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,13 +6,18 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ProductsComponent } from './products/products.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavigationComponent } from './components/header/navigation/navigation.component';
+import { SidebarComponent } from './components/header/sidebar/sidebar.component';
+import { LogoComponent } from './components/logo/logo.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent, ProductsComponent
+    AppComponent, ProductsComponent, HeaderComponent, NavigationComponent, SidebarComponent, LogoComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule, AppRoutingModule, RouterModule
+    BrowserModule, HttpClientModule, AppRoutingModule, RouterModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
