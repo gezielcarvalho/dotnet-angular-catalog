68b11f16194cbc44184fa90591213462728336bf
Geziel Carvalho <geziel.natal@gmail.com>
2023-12-12 14:58:25 -0500

    change attribute to optional

--- backend/Models/Product.cs
+++ backend/Models/Product.cs
@@ -15,7 +15,7 @@ namespace Backend.Models
 
         public string Name { get; set; } = null!;
         public string Description { get; set; } = null!;
-        public string ImgUrl { get; set; } = null!;
+        public string? ImgUrl { get; set; } = null!;
         public double Price { get; set; }
         public DateTime? CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
