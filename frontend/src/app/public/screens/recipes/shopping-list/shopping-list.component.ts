4fba0d53892941cb24863bd1636136e442d904db
Geziel Carvalho <geziel.natal@gmail.com>
2023-12-30 17:18:00 -0500

    add edit form

--- frontend/src/app/public/screens/recipes/shopping-list/shopping-list.component.ts
+++ frontend/src/app/public/screens/recipes/shopping-list/shopping-list.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
+import { Ingredient } from 'src/app/shared/models/ingredients.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
     standalone: true,
-    imports: [ShoppingEditComponent],
+    imports: [ShoppingEditComponent, CommonModule],
     selector: 'app-shopping-list',
     templateUrl: './shopping-list.component.html',
 })
 export class ShoppingListComponent implements OnInit {
-    ingredients = [];
+    ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10),
+    ];
     constructor() {}
     ngOnInit() {
         // TODO
