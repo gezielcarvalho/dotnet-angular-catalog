1fd96df33a27e2ce9de29490e407a5a8c45daf3e
Geziel Carvalho <geziel.natal@gmail.com>
2023-12-30 09:50:30 -0500

    set recipe list layout

--- frontend/src/app/components/public/recipes/recipe-list/recipe-list.component.ts
+++ frontend/src/app/components/public/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RecipeItemComponent } from './recipe-item/recipe-item.component';
+import { Recipe } from '../recipe.model';
 
 @Component({
     standalone: true,
@@ -8,7 +9,18 @@ import { RecipeItemComponent } from './recipe-item/recipe-item.component';
     templateUrl: './recipe-list.component.html',
 })
 export class RecipeListComponent implements OnInit {
-    recipes = [];
+    recipes: Recipe[] = [
+        new Recipe(
+            'A Test Recipe',
+            'This is simply a test',
+            'https://www.maxpixel.net/static/photo/1x/Recipe-Template-Recipe-Book-Blank-Cookbook-Blank-2169308.jpg',
+        ),
+        new Recipe(
+            'A Test Recipe',
+            'This is simply a test',
+            'https://www.maxpixel.net/static/photo/1x/Recipe-Template-Recipe-Book-Blank-Cookbook-Blank-2169308.jpg',
+        ),
+    ];
     constructor() {}
     ngOnInit() {
         // TODO
