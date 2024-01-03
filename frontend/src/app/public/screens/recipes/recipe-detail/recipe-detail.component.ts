75395c50178163e15cf34d48586f805ff90cb504
Geziel Carvalho <geziel.natal@gmail.com>
2024-01-03 09:18:52 -0500

    add ingredients to shopping list

--- frontend/src/app/public/screens/recipes/recipe-detail/recipe-detail.component.ts
+++ frontend/src/app/public/screens/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,24 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { MatMenuModule } from '@angular/material/menu';
 import { Recipe } from 'src/app/shared/models/recipe.model';
+import { RecipeService } from 'src/app/shared/services/recipe.service';
 
 @Component({
     standalone: true,
-    imports: [MatMenuModule],
+    imports: [MatMenuModule, CommonModule],
     selector: 'app-recipe-detail',
     templateUrl: './recipe-detail.component.html',
-    styles: [
-        `
-            .recipe-card {
-                @apply border-2 rounded-md items-center justify-between p-4 m-2 border-b border-gray-200;
-            }
-        `,
-    ],
+    styleUrls: ['./recipe-detail.component.css'],
 })
 export class RecipeDetailComponent implements OnInit {
     @Input() recipe: Recipe | undefined;
-    constructor() {}
+    constructor(private service: RecipeService) {}
     ngOnInit() {
         // TODO
+    }
+
+    onAddToShoppingList() {
+        this.service.addIngredientsToShoppingList(this.recipe!.ingredients);
+    }
+
+    onEditRecipe() {
+        console.log('RecipeDetailComponent.onEditRecipe');
+    }
+
+    onDeleteRecipe() {
+        console.log('RecipeDetailComponent.onDeleteRecipe');
     }
 }
--- frontend/src/app/shared/services/recipe.service.ts
+++ frontend/src/app/shared/services/recipe.service.ts
@@ -1,5 +1,7 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from '../models/recipe.model';
+import { Ingredient } from '../models/ingredients.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Injectable({
     providedIn: 'root',
@@ -11,16 +13,24 @@ export class RecipeService {
             'A Test Recipe 1',
             'This is simply a test one',
             'https://picsum.photos/id/237/200/300',
+            [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)],
         ),
         new Recipe(
             'A Test Recipe 2',
             'This is simply a test two',
             'https://picsum.photos/id/236/200/300',
+            [new Ingredient('Buns', 2), new Ingredient('Meat', 1)],
         ),
     ];
-    constructor() {}
+    constructor(private shoppingListService: ShoppingListService) {}
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients?: Ingredient[]) {
+        ingredients?.forEach(ingredient => {
+            this.shoppingListService.addIngredient(ingredient);
+        });
     }
 }
