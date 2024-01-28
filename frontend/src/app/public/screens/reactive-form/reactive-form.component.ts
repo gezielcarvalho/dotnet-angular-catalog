a39c50154920237d5eed96bd602e67ebc1b49228
Geziel Carvalho <geziel.natal@gmail.com>
2024-01-28 13:25:03 -0500

    create react form

--- frontend/src/app/public/screens/reactive-form/reactive-form.component.ts
+++ frontend/src/app/public/screens/reactive-form/reactive-form.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+    FormControl,
+    FormGroup,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms';
 
 @Component({
     standalone: true,
+    imports: [ReactiveFormsModule],
     selector: 'app-reactive-form',
     templateUrl: './reactive-form.component.html',
 })
-export class ReactiveFormComponent {}
+export class ReactiveFormComponent implements OnInit {
+    genders = ['male', 'female', 'other'];
+    signupForm!: FormGroup;
+    ngOnInit(): void {
+        this.signupForm = new FormGroup({
+            username: new FormControl(null, Validators.required),
+            email: new FormControl(null, [
+                Validators.required,
+                Validators.email,
+            ]),
+            gender: new FormControl('female'),
+        });
+    }
+}
