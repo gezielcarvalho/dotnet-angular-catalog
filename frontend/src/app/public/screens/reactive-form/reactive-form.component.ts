b52bff5827ccd53bf0ec04979e1d407808b1484a
Geziel Carvalho <geziel.natal@gmail.com>
2024-02-07 20:56:42 -0500

    Refactor reactive form component to use nested form groups and form arrays

--- frontend/src/app/public/screens/reactive-form/reactive-form.component.ts
+++ frontend/src/app/public/screens/reactive-form/reactive-form.component.ts
@@ -1,6 +1,7 @@
 import { NgFor, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import {
+    FormArray,
     FormControl,
     FormGroup,
     ReactiveFormsModule,
@@ -14,6 +15,11 @@ import {
     templateUrl: './reactive-form.component.html',
 })
 export class ReactiveFormComponent implements OnInit {
+    onAddHobby() {
+        (<FormArray>this.signupForm.get('hobbies'))?.push(
+            new FormControl(null),
+        );
+    }
     onSubmit() {
         console.log(this.signupForm.value);
     }
@@ -21,12 +27,15 @@ export class ReactiveFormComponent implements OnInit {
     signupForm!: FormGroup;
     ngOnInit(): void {
         this.signupForm = new FormGroup({
-            username: new FormControl(null, Validators.required),
-            email: new FormControl(null, [
-                Validators.required,
-                Validators.email,
-            ]),
+            userData: new FormGroup({
+                username: new FormControl(null, Validators.required),
+                email: new FormControl(null, [
+                    Validators.required,
+                    Validators.email,
+                ]),
+            }),
             gender: new FormControl('female'),
+            hobbies: new FormArray([]),
         });
     }
 }
